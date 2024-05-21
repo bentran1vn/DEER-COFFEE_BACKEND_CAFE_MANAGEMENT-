d7ce84b534f897b94ed33b17436356a166fa9480
Phuc Nghi <meocodien1762003@gmail.com>
2024-05-21 12:26:36 +0700

    update ApplicationSecurityConfiguration

--- DeerCoffeeShop.API/Configuration/ApplicationSecurityConfiguration.cs
+++ DeerCoffeeShop.API/Configuration/ApplicationSecurityConfiguration.cs
@@ -3,7 +3,9 @@ using DeerCoffeeShop.Application.Common.Interfaces;
 using DeerCoffeeShop.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace DeerCoffeeShop.Configuration
 {
@@ -18,17 +20,35 @@ namespace DeerCoffeeShop.Configuration
             JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
             services.AddHttpContextAccessor();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(
-                    JwtBearerDefaults.AuthenticationScheme,
-                    options =>
-                    {
-                        options.Authority = configuration.GetSection("Security.Bearer:Authority").Get<string>();
-                        options.Audience = configuration.GetSection("Security.Bearer:Audience").Get<string>();
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddJwtBearer(
+            //        JwtBearerDefaults.AuthenticationScheme,
+            //        options =>
+            //        {
+            //            options.Authority = configuration.GetSection("Security.Bearer:Authority").Get<string>();
+            //            options.Audience = configuration.GetSection("Security.Bearer:Audience").Get<string>();
 
-                        options.TokenValidationParameters.RoleClaimType = "role";
-                        options.SaveToken = true;
-                    });
+            //            options.TokenValidationParameters.RoleClaimType = "role";
+            //            options.SaveToken = true;
+            //        });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateIssuerSigningKey = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = configuration.GetSection("Security.Bearer:Authority").Get<string>(),
+                        ValidAudience = configuration.GetSection("Security.Bearer:Audience").Get<string>(),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Deer Coffee Shop @P!")),
+                    };
+                });
 
             services.AddAuthorization(ConfigureAuthorization);
 
